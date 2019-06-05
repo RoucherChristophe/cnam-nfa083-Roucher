07c263b0d4d41033a89ed9d6c2730b67d01c8b87
‘RoucherChristophe’ <c.roucher@orange.fr>
2019-06-05 22:41:27 +0200

    création robots.txt et sitemap.xml

--- inc/connexion.inc.php
+++ inc/connexion.inc.php
@@ -5,11 +5,11 @@ $pdo_options[PDO::MYSQL_ATTR_INIT_COMMAND]= 'SET NAMES utf8';
 $serveur = "localhost";
 $loginserveur = "root";
 $mdpserveur = "";
-/* locale */ $serveur = "localhost"; $bddname = "cnamcp09_nfa083cf1_2018";
-/* locale */ $loginserveur = "root"; $mdpserveur = "";
+/* locale */ //$serveur = "localhost"; $bddname = "cnamcp09_nfa083cf1_2018";
+/* locale */ //$loginserveur = "root"; $mdpserveur = "";
 
-/* online */// $serveur = "localhost"; $bddname = "cnamcp09_nfa083cf1_2018";
-/* online */// $loginserveur = "root"; $mdpserveur = "";
+/* online */ $serveur = "localhost"; $bddname = "id9843978_cnamcp09_nfa083cf1_2018";
+/* online */ $loginserveur = "id9843978_christophe"; $mdpserveur = "christophe";
 
 
 $con = new PDO('mysql:host='.$serveur.';dbname='.$bddname,$loginserveur,$mdpserveur,$pdo_options);
